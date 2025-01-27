1e4fd670bde64f431757e2ea2ffe31620b1d4f5b
Parthik Diyora <ddiyora503@gmail.com>
2025-01-27 21:47:48 +0530

    sidebar update

--- src/home/HeaderSocials.jsx
+++ src/home/HeaderSocials.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { SiCodeforces } from "react-icons/si";
+import { SiLeetcode } from "react-icons/si";
 
 const HeaderSocials = () => {
   return (
@@ -8,13 +10,13 @@ const HeaderSocials = () => {
           <i class="fa-brands fa-linkedin"></i>
         </a>
         <a href="/" className="home__social-link" target='_blank'>
-          <i class="fa-brands fa-x-twitter"></i>
+          <SiLeetcode />
         </a>
         <a href="/" className="home__social-link" target='_blank'>
           <i class="fa-brands fa-github"></i>
         </a>
         <a href="/" className="home__social-link" target='_blank'>
-          <i class="fa-brands fa-instagram"></i>
+          <SiCodeforces />
         </a>
       </div>
 
--- src/sidebar/sidebar.jsx
+++ src/sidebar/sidebar.jsx
@@ -1,70 +1,57 @@
 import React, { useState, useEffect } from "react";
+import { FaHome, FaUser, FaBriefcase, FaGraduationCap, FaLayerGroup, FaStickyNote, FaComment, FaBars } from "react-icons/fa";
 import "./sidebar.css";
 import Moon from "../assets/Moon.svg";
 import Sun from "../assets/Sun.svg";
 
 const Sidebar = () => {
     const [toggle, showMenu] = useState(false);
-    const [dark, setDark] = useState(false); // Use boolean for dark mode
+    const [dark, setDark] = useState(false);
 
-    const darktheme = () => {
-        setDark(!dark);
-    };
+    const darktheme = () => setDark(!dark);
 
     useEffect(() => {
-        document.body.className = dark ? "lignt-theme" : "dark-theme";
+        document.body.className = dark ? "light-theme" : "dark-theme";
     }, [dark]);
 
     return (
         <>
             <aside className={toggle ? "aside show-menu" : "aside"}>
                 <a href="#home" className="nav__logo">
-                    <img src={dark ? Moon : Sun} alt="" className="moon" onClick={darktheme} />
+                    <img
+                        src={dark ? Moon : Sun}
+                        alt={dark ? "Dark mode" : "Light mode"}
+                        className="moon"
+                        onClick={darktheme}
+                    />
                 </a>
 
                 <nav className="nav">
-                    <nav className="nav">
-                        <div className="nav__menu">
-                            <ul className="nav__list">
-                                <li className="nav__item">
-                                    <a href="#home" className="nav__link">
-                                        <i className="icon-home"></i>
-                                    </a>
-                                </li>
-                                <li className="nav__item">
-                                    <a href="#about" className="nav__link">
-                                        <i className="icon-user-following"></i>
-                                    </a>
-                                </li>
-                                <li className="nav__item">
-                                    <a href="#services" className="nav__link">
-                                        <i className="icon-briefcase"></i>
-                                    </a>
-                                </li>
-
-                                <li className="nav__item">
-                                    <a href="#resume" className="nav__link">
-                                        <i className="icon-graduation"></i>
-                                    </a>
-                                </li>
-                                <li className="nav__item">
-                                    <a href="#portfolio" className="nav__link">
-                                        <i className="icon-layers"></i>
-                                    </a>
-                                </li>
-                                <li className="nav__item">
-                                    <a href="#blog" className="nav__link">
-                                        <i className="icon-note"></i>
-                                    </a>
-                                </li>
-                                <li className="nav__item">
-                                    <a href="#contact" className="nav__link">
-                                        <i className="icon-bubble"></i>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
+                    <div className="nav__menu">
+                        <ul className="nav__list">
+                            <li className="nav__item">
+                                <a href="#home" className="nav__link"><FaHome /></a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="#about" className="nav__link"><FaUser /></a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="#services" className="nav__link"><FaBriefcase /></a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="#resume" className="nav__link"><FaGraduationCap /></a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="#portfolio" className="nav__link"><FaLayerGroup /></a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="#blog" className="nav__link"><FaStickyNote /></a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="#contact" className="nav__link"><FaComment /></a>
+                            </li>
+                        </ul>
+                    </div>
                 </nav>
 
                 <div className="nav__footer">
@@ -76,7 +63,7 @@ const Sidebar = () => {
                 className={toggle ? "nav__toggle nav__toggle-open" : "nav__toggle"}
                 onClick={() => showMenu(!toggle)}
             >
-                <i className="icon-menu"></i>
+                <FaBars />
             </div>
         </>
     );
